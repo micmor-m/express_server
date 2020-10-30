b021d817d69469f2135724fd3d00ba0308e944b2
mic <micmor.m@gmail.com>
2020-10-30 11:53:57 -0400

    update routes files

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 //import core module
-const http = require("http");
+//const http = require("http");
 
 //import 3rd party packages
 const express = require("express");
+const bodyParser = require("body-parser");
 
 //create express application
 const app = express();
 
-//create server
-const server = http.createServer(app);
+//import my functions
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+//use body parser (works with HTML form and JSON but not with file)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//can be use as any middleware
+//the order of the routes still matter
+app.use(adminRoutes);
+
+app.use("/", (req, res, next) => {
+  console.log("I always run!");
+  next();
+});
+
+app.use(shopRoutes);
 
 //start server
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  console.log("I am the first middleware!");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.export = router;
