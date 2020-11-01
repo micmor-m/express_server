076246f473a25a056de9a309248c261c7229f6a9
mic <micmor.m@gmail.com>
2020-10-31 22:43:40 -0400

    update style

--- helper/path.js
+++ helper/path.js
@@ -1,2 +1,7 @@
+//for old version of express
 // const path = require("path");
 // module.exports = path.dirname(process.mainModule.filename);
+
+//for new version of express
+const path = require("path");
+module.exports = path.dirname(require.main.filename);
